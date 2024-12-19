28f9f39991212017e04edc26bc4ad2a421b5330c
佐田 龍之介 <suc1842@insource.local>
2024-12-19 15:20:04 +0900

    my 2st commit

--- db.php
+++ db.php
@@ -1,38 +1,58 @@
 <?php
-
 try {
     // データベース接続
     $pdo = new PDO("mysql:host=localhost; dbname=sata_db; charset=utf8mb4", 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     
-    // 勝ち数、負け数、引き分け数を集計するクエリ
+    // クエリ
     $stmt = $pdo->query("
         SELECT
-            winner,
-            COUNT(*) AS count
+            my_turn,
+            result
         FROM
             tic_tac_toe_results
-        GROUP BY
-            winner
     ");
     
-    // 集計結果を取得
+    // 結果を取得
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-    // 初期化
-    $playerWins = 0; // 〇（プレイヤー）の勝利数
-    $cpuWins = 0; // ✕（CPU）の勝利数
-    $draws = 0;  // 引き分け数
+    // 先攻
+    $firstWins = 0; // 先攻勝ち
+    $firstLose = 0; // 先攻負け
+    $firstdraws = 0; // 先攻引き分け
+    
+    // 後攻
+    $secondWins = 0; // 後攻勝ち
+    $secondLose = 0; // 後攻負け
+    $seconddraws = 0; // 後攻引き分け
     
     // 結果を分類
     foreach ($results as $row) {
-        if ($row['winner'] === 'O') {
-            $playerWins = $row['count'];
-        } elseif ($row['winner'] === 'X') {
-            $cpuWins = $row['count'];
-        } elseif ($row['winner'] === 'DRAW') {
-            $draws = $row['count'];
+        
+        if ($row['result'] === 'circle' && $row['my_turn'] === 'first') { // 先攻勝ち
+            $f_win[] = $row['result'];
+            $firstWins = count($f_win);
+            
+        } elseif ($row['result'] === 'cross' && $row['my_turn'] === 'first') { // 先攻負け
+            $f_lose[] = $row['result'];
+            $firstLose = count($f_lose);
+            
+        } elseif ($row['result'] === 'cross' && $row['my_turn'] === 'second') { // 後攻勝ち
+            $s_win[] = $row['result'];
+            $secondWins = count($s_win);
+            
+        } elseif ($row['result'] === 'circle' && $row['my_turn'] === 'second') { // 後攻負け
+            $s_lose[] = $row['result'];
+            $secondLose = count($s_lose);
+            
+        } elseif ($row['result'] === 'DRAW' && $row['my_turn'] === 'first') { // 先攻引き分け
+            $f_drawa[] = $row['result'];
+            $firstdraws = count($f_drawa);
+            
+        } elseif ($row['result'] === 'DRAW' && $row['my_turn'] === 'second') { // 後攻引き分け
+            $s_draws[] = $row['result'];
+            $seconddraws = count($s_draws);
         }
     }
 } catch (PDOException $Exception) {
--- index.php
+++ index.php
@@ -2,90 +2,117 @@
 session_start();
 require_once 'db.php';
 
-// ゲームスタート！
-if (isset($_POST['start'])) {
-    $_SESSION['Player'] = '〇'; // 先行のプレイヤー
+// 初期関数
+function start($turn)
+{
+    $_SESSION['Player'] = $turn; // プレイヤーの設定
+    $_SESSION['computer'] = ""; // 後攻時使用
+
     for ($i = 0; $i < 9; $i ++) {
-        $_SESSION["cell_$i"] = null; // 9つのセルの初期化
+        $_SESSION["cell_$i"] = null; // マスの初期化
     }
-    $_SESSION["count"] = 0; // カウントを初期化
     $_SESSION['message'] = null;
+
+    // 後攻の場合、コンピュータが最初に動く
+    if ($turn === 'cross') {
+        $_SESSION['computer'] = "value";
+        computerMove();
+        switchPlayer();
+    }
 }
 
-// プレイヤーの初期化を確認
-if (! isset($_SESSION['Player'])) {
-    $_SESSION['Player'] = '〇'; // セッションにプレイヤー情報がなければデフォルト値を設定
+// 先攻ボタンクリック時
+if (isset($_POST['circle_btn'])) {
+    $_SESSION['move'] = 'first';
+    start("circle");
 }
 
-// プレイヤーの〇✕を切り替える
+// 後攻ボタンクリック時
+if (isset($_POST['cross_btn'])) {
+    $_SESSION['move'] = 'second';
+    start("cross");
+}
+
+// 先攻・後攻を決めずにクリックした場合
+if (! isset($_SESSION['Player'])) { 
+    $_SESSION['Player'] = ""; 
+}
+
+// プレイヤーのcircle〇、cross✕を切り替える
 function switchPlayer()
 {
-    if (isset($_SESSION['Player']) && $_SESSION['Player'] === '〇') {
-        $_SESSION['Player'] = '✕';
+    if ($_SESSION['Player'] === "circle") {
+        $_SESSION['Player'] = "cross";
     } else {
-        $_SESSION['Player'] = '〇';
+        $_SESSION['Player'] = "circle";
     }
 }
 
-// セルの表示変更
-if ($_SERVER['REQUEST_METHOD'] === 'POST') { // サーバー情報および実行時の環境情報を取得する
+// コンピュータの動き
+function computerMove()
+{
+    // nullのマスを探して、配列に入れる
+    $emptyCells = [];
     for ($i = 0; $i < 9; $i ++) {
-        $cellKey = "cell_$i";
-
-        // セッションキーが未設定の場合は初期化する
-        if (! isset($_SESSION[$cellKey])) {
-            $_SESSION[$cellKey] = null;
+        if ($_SESSION["cell_$i"] === null) {
+            $emptyCells[] = $i;
         }
+    }
+    
+    // 配列からランダムにindexを取り出す
+    if (! empty($emptyCells)) {
+        $randIndex = array_rand($emptyCells);
+        $compCellKey = "cell_" . $emptyCells[$randIndex];
 
-        if (isset($_POST[$cellKey]) && $_SESSION[$cellKey] === null) {
-            $_SESSION[$cellKey] = $_SESSION['Player']; // 現在のプレイヤーの〇✕を代入
-            switchPlayer(); // プレイヤーを切り替え
+        if ($_SESSION['computer'] === "value") {
+            $_SESSION[$compCellKey] = "circle"; // コンピュータ（〇）
+        } else
+            $_SESSION[$compCellKey] = "cross"; // コンピュータ（✕）
+    }
+}
 
-            // 相手のターン
-            // まずは、cell０～９のうち、nullを探し、配列に入れる。
-            $emptyCells = [];
-            for ($j = 0; $j < 9; $j ++) {
-                if (! isset($_SESSION["cell_$j"])) {
-                    $_SESSION["cell_$j"] = null;
-                }
-                if ($_SESSION["cell_$j"] === null) {
-                    $emptyCells[] = $j;
-                }
+// マスの表示変更
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // 先攻・後攻を決めずにクリックした場合
+    if ($_SESSION['Player'] === "") {
+        $_SESSION['message'] = "先攻か後攻を選択してください！";
+        $_SESSION['message'] = styleMessage_2($_SESSION['message']);
+    } else {
+        
+        // プレイヤーの動き
+        for ($i = 0; $i < 9; $i ++) {
+            $cellKey = "cell_$i";
+            
+            
+            if (! isset($_SESSION[$cellKey])) { // 設定されていない場合、初期化
+                $_SESSION[$cellKey] = null;
             }
-            // さっき作った配列からランダムにインデックスをとりだす。
-            // もし、$emptyCellsが空ではなかったら？
-            if (! empty($emptyCells)) {
-                $randIndex = array_rand($emptyCells);
-                $compCellKey = "cell_" . $emptyCells[$randIndex];
-                $_SESSION[$compCellKey] = $_SESSION['Player'];
 
+            // クリックしたマスが空白の場合
+            if (isset($_POST[$cellKey]) && $_SESSION[$cellKey] === null) {
+ 
+                if ($_SESSION['computer'] === "value") { // 後攻の場合
+                    $_SESSION[$cellKey] = "cross";
+                } else
+                    $_SESSION[$cellKey] = "circle"; // 先攻の場合
                 $winner = checkWinner();
 
                 if ($winner) {
-                    if ($winner === 'draw') {
-                        $_SESSION['message'] = '引き分けです！';
-                    } else {
-                        $_SESSION['message'] = "勝者は{$winner}！";
+                    gameEnd($winner);
+                    break;
+                }
 
-                        $_SESSION['message'] = styleMessage($_SESSION['message']);
-                    }
+                switchPlayer(); // プレイヤーからコンピュータへ切り替え
+                computerMove(); // コンピュータのターン
+
+                $winner = checkWinner();
+                if ($winner) {
+                    gameEnd($winner);
                 } else {
-                    switchPlayer();
+                    switchPlayer(); // プレイヤーのターンに切り替え
                 }
-                break; // クリックされたセルだけを更新
-
-                // $emptyCellsが空→つまり、マスに空きがない
-            } else {
-                $winner = checkWinner();
-                if ($winner) {
-                    if ($winner === 'draw') {
-                        $_SESSION['message'] = '引き分けだ！';
-                    } else {
-                        $_SESSION['message'] = "勝者は{$winner}！";
-
-                        $_SESSION['message'] = styleMessage($_SESSION['message']);
-                    }
-                }
+                break;
             }
         }
     }
@@ -102,49 +129,73 @@ function checkWinner()
 
     foreach ($winningPatterns as $pattern) {
         [$a,$b,$c] = $pattern;
-        if (isset($_SESSION["cell_$a"], $_SESSION["cell_$b"], $_SESSION["cell_$c"]) && $_SESSION["cell_$a"] !== null && $_SESSION["cell_$a"] === $_SESSION["cell_$b"] && $_SESSION["cell_$b"] === $_SESSION["cell_$c"]) {
+        if (isset($_SESSION["cell_$a"], $_SESSION["cell_$b"], $_SESSION["cell_$c"]) &&
+            $_SESSION["cell_$a"] !== null && $_SESSION["cell_$b"] !== null && $_SESSION["cell_$c"] !== null &&
+            $_SESSION["cell_$a"] === $_SESSION["cell_$b"] && $_SESSION["cell_$b"] === $_SESSION["cell_$c"]) {
 
-            $winner = $_SESSION["cell_$a"]; // 勝者を取得 ('〇' または '✕')
-            saveResultToDB($winner); // 勝敗をDBに保存
-            return $winner; // 勝者を返す
+                $winner = $_SESSION["cell_$a"]; // 勝者を取得 (circle or cross)
+
+            // 空いているマスを全て空白で埋める
+            for ($i = 0; $i < 9; $i ++) {
+                if ($_SESSION["cell_$i"] === null) {
+                    $_SESSION["cell_$i"] = '';
+                }
+            }
+            $move = $_SESSION['move'];
+            saveResultDB($winner, $move); // 勝敗をDBに保存
+            return $winner;
         }
     }
 
     for ($i = 0; $i < 9; $i ++) {
+        // 勝敗判定がつかず、マスがすべて埋まった場合
         if ($_SESSION["cell_$i"] === null) {
-            return null; // ゲーム続行
+            return null;
         }
     }
-
-    saveResultToDB("DRAW"); // 引き分けの場合を保存
-    return 'draw'; // 引き分け
+    $move = $_SESSION['move'];
+    saveResultDB("DRAW", $move); // 引き分けをDBに保存
+    return 'draw';
 }
 
-// データベースに送る関数
-function saveResultToDB($winner)
+// ゲーム終了時の処理
+function gameEnd($winner)
 {
-    global $pdo; // グローバルスコープの $pdo を使用
+    if ($winner === 'draw') {
+        $_SESSION['message'] = '引き分けです！';
+    } else {
+        $_SESSION['message'] = "{$winner}の勝ち！！";
+    }
+    $_SESSION['message'] = styleMessage($_SESSION['message']);
+}
 
-    echo '保存される winner の値: ' . htmlspecialchars($winner) . '<br>';
-
+// dbに結果を保存
+function saveResultDB($winner, $move)
+{
+    global $pdo;
     try {
-        $stmt = $pdo->prepare("INSERT INTO tic_tac_toe_results (winner) VALUES (:winner)");
+        $stmt = $pdo->prepare("INSERT INTO tic_tac_toe_results (result, my_turn) VALUES (:result, :my_turn)");
         $stmt->execute([
-            ":winner" => $winner
+            ':result' => $winner,
+            ':my_turn' => $move
         ]);
     } catch (PDOException $e) {
-        error_log('データベースエラー: ' . $e->getMessage()); // エラーを記録
-        echo 'データベースエラー: ' . htmlspecialchars($e->getMessage());
+        error_log('データベースエラー: ' . $e->getMessage());
     }
 }
 
 // メッセージを装飾する関数
 function styleMessage($message)
 {
-    // 〇を青、✕を赤にする
-    $message = preg_replace('/〇/', '<span class="blue">〇</span>', $message);
-    $message = preg_replace('/✕/', '<span class="red">✕</span>', $message);
+    $message = preg_replace('/circle/', '<span class="blue">〇</span>', $message);
+    $message = preg_replace('/cross/', '<span class="red">✕</span>', $message);
+    return $message;
+}
 
+function styleMessage_2($message)
+{
+    $message = preg_replace('/先攻/', '<span class="blue">先攻</span>', $message);
+    $message = preg_replace('/後攻/', '<span class="red">後攻</span>', $message);
     return $message;
 }
 
@@ -152,10 +203,9 @@ function styleMessage($message)
 if (isset($_POST['reset'])) {
     session_unset();
     session_destroy();
-    session_start();
-    $_SESSION['message'] = null;
 }
 ?>
+
 <!doctype html>
 <html lang="ja">
 <head>
@@ -164,7 +214,6 @@ if (isset($_POST['reset'])) {
 <title>三目並べ</title>
 <link rel="stylesheet" href="../Tictactoe/style.css">
 </head>
-
 <body>
   <header>
     <div class="header-content">
@@ -176,53 +225,71 @@ if (isset($_POST['reset'])) {
 
   <div class="sub_main">
     <div class="display">
-      <p class="start"><?php if(isset($_POST['start'])) {echo "試合開始！";} ?></p>
+      <p class="start"><?php if(isset($_POST['circle_btn'])) {echo "あなたは'<span class='blue'>" . htmlspecialchars('先攻') . "</span>'です";} ?></p>
+      <p class="start"><?php if(isset($_POST['cross_btn'])) {echo "あなたは'<span class='red'>" . htmlspecialchars('後攻') . "</span>'です";} ?></p>
       <p class="message"><?php echo isset($_SESSION['message']) ? $_SESSION['message'] : ''; ?></p>
     </div>
   </div>
 
   <div class="main">
     <form method="post">
-      <div class="board">
 
-        
+      <div class="board">
         <?php for ($i = 0; $i < 9; $i ++) :?>
           <div class="cell">
-          <button type="submit" class="btn" name="cell_<?php echo $i; ?>">
+          <button type="submit" class="btn" name="cell_<?php echo htmlspecialchars($i); ?>">
             <?php
             if (isset($_SESSION["cell_$i"])) {
-                echo "<span class='mark'>{$_SESSION["cell_$i"]}</span>";
+                if ($_SESSION["cell_$i"] === "circle") {
+                    echo "<span class='mark circle-style'>〇</span>";
+                } else if ($_SESSION["cell_$i"] === "cross") {
+                    echo "<span class='mark cross-style'>✕</span>";
+                }
             }
             ?>
             </button>
         </div>
         <?php endfor; ?>
-
       </div>
 
       <div class="sr_btn">
-        <button type="submit" name="start" class="s_btn">ゲームスタート！</button>
-        <button type="submit" name="reset" class="r_btn">リセット</button>
+        <button type="submit" name="circle_btn" class="circle_btn">先<br></button>
+        <button type="submit" name="cross_btn" class="cross_btn">後</button>
+        <button type="submit" name="reset" class="s_btn">リセット</button>
       </div>
 
       <table>
         <tbody>
           <tr>
-            <th>勝利数</th>
-            <th>〇(プレイヤー)</th>
-            <th>✕（CPU）</th>
+            <th>累計</th>
+            <th>勝ち</th>
+            <th>負け</th>
             <th>引き分け</th>
           </tr>
           <tr>
-            <th>通算</th>
-            <td><?php echo $playerWins; ?></td>
-            <td><?php echo $cpuWins; ?></td>
-            <td><?php echo $draws; ?></td>
+            <th class="first">先攻</th>
+            <td><?php echo htmlspecialchars($firstWins, ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars($firstLose, ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars($firstdraws, ENT_QUOTES, 'UTF-8'); ?></td>
+          </tr>
+          <tr>
+            <th class="second">後攻</th>
+            <td><?php echo htmlspecialchars($secondWins, ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars($secondLose, ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars($seconddraws, ENT_QUOTES, 'UTF-8'); ?></td>
           </tr>
         </tbody>
       </table>
 
     </form>
+    
+    <div class="explanation">
+    <p>※遊び方<br>
+    1.先攻ボタンまたは後攻ボタンのいずれかをクリックしてください。<br>
+    2.空いているマスをクリックすると、〇または✕が表示されます。<br>
+    3.ゲームが終了したら、リセットボタンをクリックして再挑戦しましょう。<br></p>
+    </div>
+    
   </div>
 
   <footer>
@@ -230,16 +297,5 @@ if (isset($_POST['reset'])) {
       <small>Copyright © sata Co., Ltd. All rights reserved.</small>
     </div>
   </footer>
-
-  <script src="js/script.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-  <script>
-$(document).ready(function(){
-    $(".mark:contains('〇')").addClass("circle-style");
-    $(".mark:contains('✕')").addClass("cross-style");
-});
-</script>
-
 </body>
 </html>
